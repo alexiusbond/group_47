428b638078a4d6ce62e002b90e0d1109a629664f
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2024-10-14 18:39:48 +0600

    add decouple

--- lesson_5.py
+++ lesson_5.py
@@ -1,8 +1,10 @@
 # import random
+from email.policy import default
 from random import randint as generate_number
 from utils import calculator as calc
 from utils.person import Student
 from termcolor import cprint
+from decouple import config
 
 # print(random.randint(1, 6))
 print(generate_number(1, 6))
@@ -11,3 +13,7 @@ print(calc.addition(9, 1))
 my_friend = Student('Kevin', 29)
 print(my_friend)
 cprint("Hello, World!", "green", "on_red")
+
+print(config('DATABASE_URL'))
+commented = config('COMMENTED', default='0', cast=int)
+print(commented * 2)
