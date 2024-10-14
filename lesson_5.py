d2840c4b9f493bb9131505c756667338aaf4ff77
alexiusbond <aleksey.bondarev@iaau.edu.kg>
2024-10-14 18:21:01 +0600

    lesson_5

---
+++ lesson_5.py
@@ -0,0 +1,13 @@
+# import random
+from random import randint as generate_number
+from utils import calculator as calc
+from utils.person import Student
+from termcolor import cprint
+
+# print(random.randint(1, 6))
+print(generate_number(1, 6))
+print(calc.addition(9, 1))
+
+my_friend = Student('Kevin', 29)
+print(my_friend)
+cprint("Hello, World!", "green", "on_red")
